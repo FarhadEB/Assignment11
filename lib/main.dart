64f3c2c505d4ffc25282108c697a33eac7152196
Farhad EB <farhadebrahimzadeh14@gmail.com>
2024-12-09 15:43:10 +0430

    Update main.dart
    New Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_application_1/Pages.dart';
+import 'package:flutter_application_1/pages.dart';
 import 'dart:async';
 
 void main(List<String> args) {
@@ -31,7 +31,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     Future.delayed(Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (context) => MainApp()),
+        MaterialPageRoute(builder: (context) => MainApp()),
       );
     });
   }
@@ -67,50 +67,67 @@ class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Afghan Fruits App'),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/Fruits.png'),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(150),
+        child: Padding(
+          padding: const EdgeInsets.only(right: 20,left: 20),
+          child: AppBar(
+            title: Center(
+                child: Text(
+              'Afghan Fruits App',
+              style: TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold, fontSize: 40),
+            )),
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/Fruits.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
+             
+            ),
+            actions: [
+              PopupMenuButton<String>(
+                onSelected: (value) {
+                  switch (value) {
+                    case 'Share the app':
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Share the app clicked!')),
+                      );
+                      break;
+                    case 'About Me':
+                      SnackBar(content: Text('About Programmer'));
+                      break;
+                    case 'Exit':
+                      Navigator.of(context).pop();
+                      break;
+                  }
+                },
+                itemBuilder: (BuildContext context) {
+                  return [
+                    PopupMenuItem<String>(
+                      value: 'Share the app',
+                      child: Text('Share the app'),
+                    ),
+                    PopupMenuItem<String>(
+                      onTap: () {
+                        Navigator.push(
+                            context, MaterialPageRoute(builder: (c) => AboutMe()));
+                      },
+                      value: 'About',
+                      child: Text('About'),
+                    ),
+                    PopupMenuItem<String>(
+                      value: 'Exit',
+                      child: Text('Exit'),
+                    ),
+                  ];
+                },
+              ),
+            ],
           ),
         ),
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: (value) {
-              switch (value) {
-                case 'Share the app':
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Share the app clicked!')),
-                  );
-                  break;
-                case 'About Me':
-                Navigator.push(context, MaterialPageRoute(builder: (c)=>AboutMe()));
-                  break;
-                case 'Exit':
-                  Navigator.of(context).pop();
-                  break;
-              }
-            },
-            itemBuilder: (BuildContext context) {
-              return [
-                PopupMenuItem<String>(
-                  value: 'Share the app',
-                  child: Text('Share the app'),
-                ),
-                PopupMenuItem<String>(
-                  value: 'About',
-                  child: Text('About'),
-                ),
-                PopupMenuItem<String>(
-                  value: 'Exit',
-                  child: Text('Exit'),
-                ),
-              ];
-            },
-          ),
-        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -123,55 +140,71 @@ class _MainAppState extends State<MainApp> {
           children: [
             GestureDetector(
               onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) =>Apple()));
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Apple()));
               },
               child: Container(
                 color: Colors.blue,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/Apple.png'),
-                      radius: 80,
+                    Hero(
+                      tag: 'Apple',
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/Apple.png'),
+                        radius: 80,
+                      ),
                     ),
                     SizedBox(
                       height: 20,
                     ),
                     Text(
                       'Apple',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-              GestureDetector(
+            GestureDetector(
               onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) =>Pomegranate()));
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Pomegranate()));
               },
               child: Container(
                 color: Colors.blue,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/Pomegranate.png'),
-                      radius: 80,
+                    Hero(
+                      tag: 'Pomegranate',
+                      child: CircleAvatar(
+                        backgroundImage:
+                            AssetImage('assets/images/Pomegranate.png'),
+                        radius: 80,
+                      ),
                     ),
                     SizedBox(
                       height: 20,
                     ),
                     Text(
                       'Pomegranate',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-             GestureDetector(
+            GestureDetector(
               onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) =>Fig()));
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Fig()));
               },
               child: Container(
                 color: Colors.blue,
@@ -187,15 +220,19 @@ class _MainAppState extends State<MainApp> {
                     ),
                     Text(
                       'Fig',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-             GestureDetector(
+            GestureDetector(
               onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) =>Orange()));  
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Orange()));
               },
               child: Container(
                 color: Colors.blue,
@@ -211,15 +248,19 @@ class _MainAppState extends State<MainApp> {
                     ),
                     Text(
                       'Orange',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-             GestureDetector(
+            GestureDetector(
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>Melon()));
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Melon()));
               },
               child: Container(
                 color: Colors.blue,
@@ -235,16 +276,19 @@ class _MainAppState extends State<MainApp> {
                     ),
                     Text(
                       ' Melon',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-             GestureDetector(
+            GestureDetector(
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>WaterMelon()));
-                
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WaterMelon()));
               },
               child: Container(
                 color: Colors.blue,
@@ -252,7 +296,8 @@ class _MainAppState extends State<MainApp> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/WaterMelon.png'),
+                      backgroundImage:
+                          AssetImage('assets/images/WaterMelon.png'),
                       radius: 80,
                     ),
                     SizedBox(
@@ -260,15 +305,19 @@ class _MainAppState extends State<MainApp> {
                     ),
                     Text(
                       'Water Melon',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-             GestureDetector(
+            GestureDetector(
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>Grape()));
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Grape()));
               },
               child: Container(
                 color: Colors.blue,
@@ -284,16 +333,19 @@ class _MainAppState extends State<MainApp> {
                     ),
                     Text(
                       'Grape',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-             GestureDetector(
+            GestureDetector(
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>Berry()));
-                
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Berry()));
               },
               child: Container(
                 color: Colors.blue,
@@ -309,16 +361,19 @@ class _MainAppState extends State<MainApp> {
                     ),
                     Text(
                       'Berry',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-             GestureDetector(
+            GestureDetector(
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>Cherry()));
-                
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Cherry()));
               },
               child: Container(
                 color: Colors.blue,
@@ -334,38 +389,46 @@ class _MainAppState extends State<MainApp> {
                     ),
                     Text(
                       'Cherry',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-             GestureDetector(
+            GestureDetector(
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) =>Peach()));
-                
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Peach()));
               },
               child: Container(
                 color: Colors.blue,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    CircleAvatar(
-                      backgroundImage: AssetImage('assets/images/Peach.png'),
-                      radius: 80,
+                    Hero(
+                      tag: 'Peach',
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/Peach.png'),
+                        radius: 80,
+                      ),
                     ),
                     SizedBox(
                       height: 20,
                     ),
                     Text(
                       'Peach',
-                      style: TextStyle(fontSize: 20),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
                     )
                   ],
                 ),
               ),
             ),
-            
           ],
         ),
       ),
@@ -373,18 +436,82 @@ class _MainAppState extends State<MainApp> {
   }
 }
 
-class AboutMe extends StatelessWidget {
+class AboutMe extends StatefulWidget {
   const AboutMe({super.key});
+
+  @override
+  State<AboutMe> createState() => _AboutMeState();
+}
+
+class _AboutMeState extends State<AboutMe> {
+
+bool isBig=false;
+bool isWhite=false;
+ toggleBig(){
+  setState(() {
+    isBig = !isBig;
+  });
+
+  toggleRed(){
+    setState(() {
+      isWhite = !isWhite;
+    });
+  }
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('''
-        Mr Farhad Mobile Developer.
-        Farhadebrahimzadeh14@gmail.com.
-        ''',style: TextStyle(fontSize: 40,color: Colors.black38,fontWeight: FontWeight.bold),
-        ),
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          'About Developer',
+          style: TextStyle(color: Colors.white, fontSize: 40),
+        )),
+        backgroundColor: Colors.pink,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: Image.asset(
+              'assets/images/me.jpg',
+              fit: BoxFit.cover,
+              width: 300,
+              height: 200,
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          AnimatedDefaultTextStyle(
+            style: TextStyle(
+              fontSize: isBig ? 20 : 16,
+              color: isWhite ? Colors.white : Colors.black,
+            ),
+            duration: Duration(microseconds: 500),
+            child: Text(
+              '''Farhad Ebrahimzadeh
+                  Flutter Developer
+                  Gmail:farhadebrahimzadeh14@gmail.com
+                  ''',
+              // style: TextStyle(
+              //     color: Colors.white,
+              //     fontSize: 20,
+              //     fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 20,),
+          ElevatedButton(onPressed: (){
+            toggleBig();
+          }, child: Text('Toggle Big')), SizedBox(height: 20,),
+          // ElevatedButton(onPressed: (){
+          // toggleWhite();
+          // }, child: Text('Toggle Red')),
+        ],
       ),
     );
   }
